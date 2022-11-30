737541468db2086383be8394886c6d17ad1914e2
Grace-aloo <graceaaloo@gmail.com>
2022-11-30 12:50:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+  function superbowlWin(record) {
+    for (let element of record){
+        if (element.result === "W")
+          return element.year
+    }
+    //return record.find((el) => {
+    //for( let result of record ){
+     //   if (el.result === "W"){
+      //      return el.year
+        //    }
+    
+      //  }})
+    
+    }
+    
+
+
+record.find(superbowlWin(record));
+
+    // record.find(superbowlWin)
+    // return record.filter(element => {
+    //     return element.result === "W"
+    //     // if (element.result === "L") {
+    //     //     console.log(element);
+    //     // }
+    // })
+
